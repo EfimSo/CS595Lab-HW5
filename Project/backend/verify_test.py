8ecf33d123b0a4cf5ed05b3455ae1300a2b569b3
EfimSo <108906225+EfimSo@users.noreply.github.com>
2025-04-26 20:32:07 +0000

    verify test

---
+++ Project/backend/verify_test.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# Assuming vk is there
+
+# Receive HEX proof string and write to binary
+    # If binary exists, erase first
+# use binary proof to verify under vk
+    # subprocess bb verify > pipe to a file
+    # read file and return true/false val 
+    # delete file 
